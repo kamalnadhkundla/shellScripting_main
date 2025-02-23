975e37c95585b4f120e2f0e4cd9bf81d4244e6b3
Giridhara venkat Vootla <giridhar@Giridharas-MacBook-Air.local>
2025-02-23 00:39:53 -0500

    commit

--- 7.find_the_server_is_running.sh
+++ 7.find_the_server_is_running.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 SERVICE_NAME=$1
 service ${SERVICE_NAME} status >/dev/null
-if [ $? -eq 0 ]; then
-    echo "${SERVICE_NAME} IS UP and running"
+if[$# -eq 0 ]; then
+    echo  provide at least one argument
 else
-    echo "${SERVICE_NAME} IS  not UP and running"
-fi
+    if [ $? -eq 0 ]; then
+        echo "${SERVICE_NAME} IS UP and running"
+    else
+        echo "${SERVICE_NAME} IS  not UP and running"
+    fi
+fi  
