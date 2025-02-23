bd432e0f9f1dbed503af75f36f8085ccba68e2d5
Giridhara venkat Vootla <giridhar@Giridharas-MacBook-Air.local>
2025-02-23 00:45:55 -0500

    commit

--- 7.find_the_server_is_running.sh
+++ 7.find_the_server_is_running.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 SERVICE_NAME=$#
 service ${SERVICE_NAME} status >/dev/null
-if [ $# -eq 0 ]; then
-    echo  "provide at least one argument"
+if [ $# -eq 0 && $# -gt 2 ]; then
+    echo  "provide at least one argument or not more than 1 argument"
 else
     if [ $? -eq 0 ]; then
         echo "${SERVICE_NAME} IS UP and running"
