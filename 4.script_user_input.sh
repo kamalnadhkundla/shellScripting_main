0d6b7280ce88eda2dc0b835d45d5318aeda1ca13
Giridhara venkat Vootla <giridhar@Giridharas-MacBook-Air.local>
2025-02-22 23:09:16 -0500

    commit

--- 4.script_user_input.sh
+++ 4.script_user_input.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 read -p " please enter aws region name to search for vpc:" REGION
 echo "the given region by the user is ${REGION}"
-aws  ec2 describe-vpcs --region ${REGION} | jq ".Vpcs[].VpcID" 
+aws  ec2 describe-vpcs --region ${REGION} | jq 
+
 #jq if jason parsergit
