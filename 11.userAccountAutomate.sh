2d7c8549829cd84ab00cc68249018af0ec9b8a9a
Giridhara venkat Vootla <giridhar@Giridharas-MacBook-Air.local>
2025-02-23 19:02:34 -0500

    commit

--- 11.userAccountAutomate.sh
+++ 11.userAccountAutomate.sh
@@ -3,4 +3,9 @@
 USERNAME=$1
 
 EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME} | cut -d ':' -f1)
- echo ${EXISTING_USER}
+
+if [ "${USERNAME}" = "${EXISTING_USER}"]; then
+    echo "${EXISTING_USER} already exist try a differnet username"
+else
+    echo "lets create the user${USERNAME}"
+fi
