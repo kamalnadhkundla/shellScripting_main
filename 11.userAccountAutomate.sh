3d01f10e4edb53252b6df097880079687b991eb9
Giridhara venkat Vootla <giridhar@Giridharas-MacBook-Air.local>
2025-02-24 01:38:34 -0500

    commit

--- 11.userAccountAutomate.sh
+++ 11.userAccountAutomate.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #,read -p "please enter username" username
 #set -x
-SLACK_WEB='https://hooks.slack.com/services/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'
-if [ $# -gt 0 ]; then
+SLACK_WEB='https://hooks.slack.com/services/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'
     USERNAME=$1
 
     EXISTING_USER=$(cat /etc/passwd | grep -i -w ${USERNAME} | cut -d ':' -f1)
@@ -17,13 +16,9 @@ if [ $# -gt 0 ]; then
         echo "${USERNAME}:${PASSWORD}" | sudo chpasswd
         passwd -e ${USERNAME}
        # echo " the username is ${USERNAME} and password is ${PASSWORD}."
-       #curl -X POST ${SLACK_WEB} -sL -H 'content-type: application/json' --data "{"text" : \"Username is: ${USERNAME}\"}" >>/dev/null
-       #curl -X POST ${SLACK_WEB} -sL -H 'content-type: application/json' --data "{"text" : \"Temporary Password is: ${PASSWORD} Reset this password immediately.\"}" >>/dev/null
-       curl -X POST "${SLACK_WEB}" -H 'Content-Type: application/json' \
-        --data "{\"text\": \"Username is: ${USERNAME}\"}" >/dev/null 2>&1
-
-       curl -X POST "${SLACK_WEB}" -H 'Content-Type: application/json' \
-         --data "{\"text\": \"Temporary Password is: ${PASSWORD}. Reset this password immediately.\"}" >/dev/null 2>&1
+       curl -X POST ${SLACK_WEB} -sL -H 'content-type: application/json' --data "{"text" : \"Username is: ${USERNAME}\"}" >>/dev/null
+       curl -X POST ${SLACK_WEB} -sL -H 'content-type: application/json' --data "{"text" : \"Temporary Password is: ${PASSWORD} Reset this password immediately.\"}" >>/dev/null
+       
     fi
 else
     echo you have given $# arugument. okka argument evuu roo
