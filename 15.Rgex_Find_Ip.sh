dc8617c3554e0a4dce9a2379c0e3350afcb4108e
giridhara123 <giridharavenkat123@gmail.com>
2025-02-26 19:39:07 -0500

    Regex

--- 15.Rgex_Find_Ip.sh
+++ 15.Rgex_Find_Ip.sh
@@ -3,7 +3,7 @@
 
 DATA=$(cat iplist.log)
 for IPADDRESS in $DATA; do
-    if [[ $IPADDRESS =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1.3}\.[0-9]{1,3}$ ]]; then
+    if [[ $IPADDRESS =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
         echo "$IPADDRESS is Valid"
     else
         echo "$IPADDRESS is INvalid"
