64bf5270c457e5f2f44f5e7d62dc683b41ba5bb1
giridhara123 <giridharavenkat123@gmail.com>
2025-02-26 19:38:33 -0500

    Regex

--- 15.Rgex_Find_Ip.sh
+++ 15.Rgex_Find_Ip.sh
@@ -1,7 +1,7 @@
 #finding ip address is valid or not using regex expression
 #!bin/bash
 
-DATA=$(cat applist.log)
+DATA=$(cat iplist.log)
 for IPADDRESS in $DATA; do
     if [[ $IPADDRESS =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1.3}\.[0-9]{1,3}$ ]]; then
         echo "$IPADDRESS is Valid"
