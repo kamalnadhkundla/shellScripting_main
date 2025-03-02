e7200bf04c1d1304a105c9e6f100c399616cf444
giridhara123 <giridharavenkat123@gmail.com>
2025-03-01 23:21:14 -0500

    Regex

--- 18.break-continue-fileinput.sh
+++ 18.break-continue-fileinput.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for WEBSITE in $(cat websites); do
-    timeout 3 netcat -zv $websites 443 >>/dev/null/ 2>&1
+    timeout 3 netcat -zv $websites 443 >> /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo "then domain $WEBSITE ia up and running"
     else
